340dbeeb4101a395bf710c0dd80ab1cb7e28b107
MohammedAliMohammedElsady <m7md3ly2015@gmail.com>
2023-06-08 14:24:23 +0300

    oracle migrations

--- Data/ModelCreatingConfigurator.cs
+++ Data/ModelCreatingConfigurator.cs
@@ -382,6 +382,192 @@ namespace Data
                     .HasColumnName("NUMBER_OF_CASES");
             });
 
+
+
+            modelBuilder.Entity<ArtSystemPrefPerDirection>().HasNoKey().ToView(null);
+            modelBuilder.Entity<ArtSystemPrefPerStatus>().HasNoKey().ToView(null);
+            modelBuilder.Entity<ArtSystemPerfPerType>().HasNoKey().ToView(null);
+            modelBuilder.Entity<ArtUserPerformancePerActionUser>().HasNoKey().ToView(null);
+            modelBuilder.Entity<ArtUserPerformPerAction>().HasNoKey().ToView(null);
+            modelBuilder.Entity<ArtUserPerformPerUserAndAction>().HasNoKey().ToView(null);
+
+            modelBuilder.Entity<ArtSystemPreformance>(entity =>
+            {
+                entity.HasNoKey();
+
+                entity.ToView("ART_SYSTEM_PERFORMANCE", "ART_DB");
+
+                entity.Property(e => e.CaseDesc)
+                    .HasMaxLength(100)
+                    .HasColumnName("CASE_DESC");
+
+                entity.Property(e => e.CaseId)
+                    .HasMaxLength(64)
+                    .HasColumnName("CASE_ID");
+
+                entity.Property(e => e.CaseRk)
+                    .HasColumnType("numeric(10, 0)")
+                    .HasColumnName("CASE_RK");
+
+                entity.Property(e => e.CaseStatus)
+                    .HasMaxLength(4000)
+                    .HasColumnName("CASE_STATUS");
+
+                entity.Property(e => e.CaseType)
+                    .HasMaxLength(4000)
+                    .HasColumnName("CASE_TYPE");
+
+                entity.Property(e => e.ClientName)
+                    .HasMaxLength(4000)
+                    .HasColumnName("CLIENT_NAME");
+
+                entity.Property(e => e.CreateDate)
+                    .HasColumnName("CREATE_DATE");
+
+                entity.Property(e => e.CreateUserId)
+                    .HasMaxLength(60)
+                    .HasColumnName("CREATE_USER_ID");
+
+                entity.Property(e => e.DurationsInDays).HasColumnName("DURATIONS_IN_DAYS");
+
+                entity.Property(e => e.DurationsInHours).HasColumnName("DURATIONS_IN_HOURS");
+
+                entity.Property(e => e.DurationsInMinutes).HasColumnName("DURATIONS_IN_MINUTES");
+
+                entity.Property(e => e.DurationsInSeconds).HasColumnName("DURATIONS_IN_SECONDS");
+
+                entity.Property(e => e.EcmLastStatusDate)
+                    .HasColumnName("ECM_LAST_STATUS_DATE");
+
+                entity.Property(e => e.HitsCount)
+                    .HasMaxLength(250)
+                    .IsUnicode(false)
+                    .HasColumnName("HITS_COUNT");
+
+                entity.Property(e => e.IdentityNum)
+                    .HasMaxLength(4000)
+                    .IsUnicode(false)
+                    .HasColumnName("IDENTITY_NUM");
+
+                entity.Property(e => e.InvestrUserId)
+                    .HasMaxLength(250)
+                    .IsUnicode(false)
+                    .HasColumnName("INVESTR_USER_ID");
+
+                entity.Property(e => e.LockedBy)
+                    .HasMaxLength(60)
+                    .HasColumnName("LOCKED_BY");
+
+                entity.Property(e => e.Priority)
+                    .HasMaxLength(4000)
+                    .HasColumnName("PRIORITY");
+
+                entity.Property(e => e.SwiftReference)
+                    .HasMaxLength(1000)
+                    .IsUnicode(false)
+                    .HasColumnName("SWIFT_REFERENCE");
+
+                entity.Property(e => e.TransactionAmount).HasColumnName("TRANSACTION_AMOUNT");
+
+                entity.Property(e => e.TransactionCurrency)
+                    .HasMaxLength(1000)
+                    .IsUnicode(false)
+                    .HasColumnName("TRANSACTION_CURRENCY");
+
+                entity.Property(e => e.TransactionDirection)
+                    .HasMaxLength(1000)
+                    .IsUnicode(false)
+                    .HasColumnName("TRANSACTION_DIRECTION");
+
+                entity.Property(e => e.TransactionType)
+                    .HasMaxLength(1000)
+                    .IsUnicode(false)
+                    .HasColumnName("TRANSACTION_TYPE");
+
+                entity.Property(e => e.UpdateUserId)
+                    .HasMaxLength(60)
+                    .HasColumnName("UPDATE_USER_ID");
+
+                entity.Property(e => e.ValidFromDate)
+                  
+                    .HasColumnName("VALID_FROM_DATE");
+            });
+
+            modelBuilder.Entity<ArtUserPerformance>(entity =>
+            {
+                entity.HasNoKey();
+
+                entity.ToView("ART_USER_PERFORMANCE", "ART_DB");
+
+                entity.Property(e => e.Action)
+                    .HasMaxLength(256)
+                    .HasColumnName("ACTION");
+
+                entity.Property(e => e.ActionUser)
+                    .HasMaxLength(60)
+                    .HasColumnName("ACTION_USER");
+
+                entity.Property(e => e.AsssignedTime)
+
+                    .HasColumnName("ASSSIGNED_TIME");
+
+                entity.Property(e => e.CaseDesc)
+                    .HasMaxLength(100)
+                    .HasColumnName("CASE_DESC");
+
+                entity.Property(e => e.CaseId)
+                    .HasMaxLength(64)
+                    .HasColumnName("CASE_ID");
+
+                entity.Property(e => e.CaseRk)
+                    .HasColumnType("numeric(10, 0)")
+                    .HasColumnName("CASE_RK");
+
+                entity.Property(e => e.CaseStatus)
+                    .HasMaxLength(4000)
+                    .HasColumnName("CASE_STATUS");
+
+                entity.Property(e => e.CaseTypeCd)
+                    .HasMaxLength(32)
+                    .HasColumnName("CASE_TYPE_CD");
+
+                entity.Property(e => e.CreateDate)
+                    .HasColumnName("CREATE_DATE");
+
+                entity.Property(e => e.CreateUserId)
+                    .HasMaxLength(60)
+                    .HasColumnName("CREATE_USER_ID");
+
+                entity.Property(e => e.DurationsInDays).HasColumnName("DURATIONS_IN_DAYS");
+
+                entity.Property(e => e.DurationsInHours).HasColumnName("DURATIONS_IN_HOURS");
+
+                entity.Property(e => e.DurationsInMinutes).HasColumnName("DURATIONS_IN_MINUTES");
+
+                entity.Property(e => e.DurationsInSeconds).HasColumnName("DURATIONS_IN_SECONDS");
+
+                entity.Property(e => e.LockedBy)
+                    .HasMaxLength(60)
+                    .HasColumnName("LOCKED_BY");
+
+                entity.Property(e => e.Priority)
+                    .HasMaxLength(4000)
+                    .HasColumnName("PRIORITY");
+
+                entity.Property(e => e.ReleasedDate)
+                    .HasColumnName("RELEASED_DATE");
+
+                entity.Property(e => e.UpdateUserId)
+                    .HasMaxLength(60)
+                    .HasColumnName("UPDATE_USER_ID");
+
+                entity.Property(e => e.ValidFromDate)
+
+                    .HasColumnName("VALID_FROM_DATE");
+            });
+
+
+
             //AML
             modelBuilder.Entity<ArtHomeAlertsPerDate>(entity =>
             {
---
+++ OracleMigrations/Migrations/20230608112237_SystemPerformanceTablesAndSts.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace OracleMigrations.Migrations
+{
+    public partial class SystemPerformanceTablesAndSts : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+         
+                
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+           
+        }
+    }
+}
