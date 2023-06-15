13140ae3cc31108a8d72934beb91cdba222f6bd7
Islam Khalil <islamkhalil314@gmail.com>
2023-06-15 16:31:24 +0300

    GO AML For SQL Done

--- Data/Constants/StoredProcs/SQLSERVERSPNames.cs
+++ Data/Constants/StoredProcs/SQLSERVERSPNames.cs
@@ -8,12 +8,14 @@ namespace Data.Constants.StoredProcs
 {
     public static class SQLSERVERSPNames
     {
+        //Sanction
         public static readonly string ST_SYSTEM_PERF_PER_DIRECTION = "[ART_DB].[ART_ST_SYSTEM_PERF_PER_DIRECTION]";
         public static readonly string ST_SYSTEM_PERF_PER_STATUS = "[ART_DB].[ART_ST_SYSTEM_PERF_PER_STATUS]";
         public static readonly string ST_SYSTEM_PERF_PER_TYPE = "[ART_DB].[ART_ST_SYSTEM_PERF_PER_TYPE]";
         public static readonly string ST_USER_PERFORMANCE_PER_ACTION_USER = "[ART_DB].[ART_ST_USER_PERFORMANCE_PER_ACTION_USER]";
         public static readonly string ST_USER_PERFORMANCE_PER_ACTION = "[ART_DB].[ART_ST_USER_PERFORMANCE_PER_ACTION]";
         public static readonly string ST_USER_PERFORMANCE_PER_USER_AND_ACTION = "[ART_DB].[ART_ST_USER_PERFORMANCE_PER_USER_AND_ACTION]";
+        //AML
         public static readonly string ART_ST_ALERT_PER_OWNER = "[ART_DB].[ART_ST_ALERT_PER_OWNER]";
         public static readonly string ART_ST_ALERTS_PER_STATUS = "[ART_DB].[ART_ST_AML_ALERTS_PER_STATUS]";
         public static readonly string ART_ST_CASES_PER_CATEGORY = "[ART_DB].[ART_ST_CASES_PER_CATEGORY]";
@@ -25,6 +27,10 @@ namespace Data.Constants.StoredProcs
         public static readonly string ART_ST_CUST_PER_TYPE = "[ART_DB].[ART_ST_CUST_PER_TYPE]";
         public static readonly string ART_ST_AML_PROP_RISK_CLASS = "[ART_DB].[ART_ST_AML_PROP_RISK_CLASS]";
         public static readonly string ART_ST_AML_RISK_CLASS = "[ART_DB].[ART_ST_AML_RISK_CLASS]";
-        
+        //GoAml
+        public static readonly string ART_ST_GOAML_REPORTS_PER_TYPE = "[ART_DB].[ART_ST_GOAML_REPORTS_PER_TYPE]";
+        public static readonly string ART_ST_GOAML_REPORTS_PER_STATUS = "[ART_DB].[ART_ST_GOAML_REPORTS_PER_STATUS]";
+        public static readonly string ART_ST_GOAML_REPORTS_PER_INDICATOR = "[ART_DB].[ART_ST_GOAML_REPORTS_PER_INDICATOR]";
+        public static readonly string ART_ST_GOAML_REPORTS_PER_CREATOR = "[ART_DB].[ART_ST_GOAML_REPORTS_PER_CREATOR]";
     }
 }
---
+++ Data/Data/ArtGoamlReportsIndicator.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Data.Data
+{
+    public partial class ArtGoamlReportsIndicator
+    {
+        public int ReportId { get; set; }
+        public string? Indicator { get; set; }
+        public string? Description { get; set; }
+    }
+}
