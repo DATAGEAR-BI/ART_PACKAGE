d1196b1e5012475fc82a738bf14899b3ec1475ae
Islam Khalil <islamkhalil314@gmail.com>
2023-08-01 11:55:28 +0300

    fixed logic for typecode

--- ART_PACKAGE/Helpers/CustomReport/KendoFiltersExtentions.cs
+++ ART_PACKAGE/Helpers/CustomReport/KendoFiltersExtentions.cs
@@ -567,20 +567,6 @@ namespace ART_PACKAGE.Helpers.CustomReportHelpers
                 case TypeCode.Double:
                 case TypeCode.Single:
                     return true;
-                case TypeCode.Empty:
-                    break;
-                case TypeCode.Object:
-                    break;
-                case TypeCode.DBNull:
-                    break;
-                case TypeCode.Boolean:
-                    break;
-                case TypeCode.Char:
-                    break;
-                case TypeCode.DateTime:
-                    break;
-                case TypeCode.String:
-                    break;
                 default:
                     return false;
             }
