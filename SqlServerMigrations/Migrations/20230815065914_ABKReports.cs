9459f225b7dcdb13573f7c2eb8da4a1deb876bd7
MohammedAliMohammedElsady <m7md3ly2015@gmail.com>
2023-08-15 11:42:37 +0300

    test

---
+++ SqlServerMigrations/Migrations/20230815065914_ABKReports.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SqlServerMigrations.Migrations
+{
+    public partial class ABKReports : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql($@"");
+            migrationBuilder.Sql($@"");
+            migrationBuilder.Sql($@"");
+            migrationBuilder.Sql($@"");
+            migrationBuilder.Sql($@"");
+            migrationBuilder.Sql($@"");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+        }
+    }
+}
