b414596760df897a16c2d3e901f06794e1902ca0
Islam Khalil <88552597+IslamKhalil314@users.noreply.github.com>
2023-09-18 15:08:34 +0300

    alll ready for testing

--- ART_PACKAGE/Hubs/ExportHub.cs
+++ ART_PACKAGE/Hubs/ExportHub.cs
@@ -1,7 +1,10 @@
-﻿using ART_PACKAGE.Helpers;
+﻿using ART_PACKAGE.Controllers;
+using ART_PACKAGE.Helpers;
 using ART_PACKAGE.Helpers.Csv;
 using ART_PACKAGE.Helpers.CustomReport;
+using Data.Data.ARTDGAML;
 using Data.Data.ECM;
+using Data.Data.SASAml;
 using Microsoft.AspNetCore.SignalR;
 
 namespace ART_PACKAGE.Hubs
@@ -10,13 +13,40 @@ namespace ART_PACKAGE.Hubs
     {
         private readonly UsersConnectionIds connections;
         private readonly ICsvExport _csvSrv;
-        private readonly EcmContext context;
+        private readonly EcmContext _db;
+        private readonly SasAmlContext _dbAml;
+        private readonly IServiceScopeFactory _serviceScopeFactory;
+        private readonly IConfiguration _configuration;
+        private readonly ArtDgAmlContext _dgaml;
+        private readonly List<string>? modules;
 
-        public ExportHub(UsersConnectionIds connections, ICsvExport csvSrv, EcmContext context)
+        public ExportHub(UsersConnectionIds connections, ICsvExport csvSrv, EcmContext context, IConfiguration configuration, IServiceScopeFactory serviceScopeFactory)
         {
             this.connections = connections;
             _csvSrv = csvSrv;
-            this.context = context;
+            _configuration = configuration;
+            _serviceScopeFactory = serviceScopeFactory;
+            modules = _configuration.GetSection("Modules").Get<List<string>>();
+            if (modules.Contains("SASAML"))
+            {
+                IServiceScope scope = _serviceScopeFactory.CreateScope();
+                SasAmlContext amlService = scope.ServiceProvider.GetRequiredService<SasAmlContext>();
+                _dbAml = amlService;
+            }
+            if (modules.Contains("ECM"))
+            {
+                IServiceScope scope = _serviceScopeFactory.CreateScope();
+                EcmContext ecmService = scope.ServiceProvider.GetRequiredService<EcmContext>();
+                _db = ecmService;
+            }
+            if (modules.Contains("DGAML"))
+            {
+                IServiceScope scope = _serviceScopeFactory.CreateScope();
+                ArtDgAmlContext dgamlService = scope.ServiceProvider.GetRequiredService<ArtDgAmlContext>();
+                _dgaml = dgamlService;
+            }
+            _configuration = configuration;
+            _serviceScopeFactory = serviceScopeFactory;
         }
         public override Task OnConnectedAsync()
         {
@@ -34,7 +64,7 @@ namespace ART_PACKAGE.Hubs
 
         public async Task Export(ExportDto<object> para, string controller)
         {
-            await _csvSrv.Export(controller, Context.User.Identity.Name, para);
+            if (nameof(SystemPerformanceController).ToLower().Replace("controller", "") == controller.ToLower()) await _csvSrv.Export<ArtSystemPerformance, SystemPerformanceController>(_db, Context.User.Identity.Name, para);
         }
 
         public override Task OnDisconnectedAsync(Exception? exception)
