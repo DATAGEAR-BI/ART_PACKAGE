a884168fbe265aaa32ff92abffbca26f7c000081
MohammedAliMohammedElsady <m7md3ly2015@gmail.com>
2023-11-02 13:57:17 +0200

    change title for systemperformance report Cases Per Trans Direction

--- ART_PACKAGE/Controllers/SystemPerformanceSummaryController.cs
+++ ART_PACKAGE/Controllers/SystemPerformanceSummaryController.cs
@@ -136,7 +136,7 @@ namespace ART_PACKAGE.Controllers
                         x.TRANSACTION_DIRECTION ??= "UNKOWN";
                         return x;
                     }).ToList(),
-                    Title = "Swift Cases Per Direction",
+                    Title = "Cases Per Trans Direction",
                     Cat = "TRANSACTION_DIRECTION",
                     Val = "TOTAL_NUMBER_OF_CASES"
                 });
