caa1f3d6fb0b984eb273ae48c9b96a8592d59463
Ehab Azab <islamkhalil314@gmail.com>
2024-04-02 17:11:56 +0200

    add get data for user pref action user

--- ART_PACKAGE/Controllers/ECM/UserPerformancePerActionUserController.cs
+++ ART_PACKAGE/Controllers/ECM/UserPerformancePerActionUserController.cs
@@ -8,6 +8,7 @@ using Data.Constants.db;
 using Data.Constants.StoredProcs;
 using Data.Data.ECM;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 
 namespace ART_PACKAGE.Controllers.ECM
 {
@@ -35,7 +36,42 @@ namespace ART_PACKAGE.Controllers.ECM
             dbType = config.GetValue<string>("dbType").ToUpper();
 
         }
+        public IActionResult GetData([FromBody] StoredReq para)
+        {
+            IEnumerable<ArtUserPerformancePerActionUser> data = Enumerable.Empty<ArtUserPerformancePerActionUser>().AsQueryable();
 
+            IEnumerable<System.Data.Common.DbParameter> summaryParams = para.procFilters.MapToParameters(dbType);
+            if (dbType == DbTypes.SqlServer)
+            {
+                data = context.ExecuteProc<ArtUserPerformancePerActionUser>(SQLSERVERSPNames.ST_USER_PERFORMANCE_PER_ACTION_USER, summaryParams.ToArray());
+            }
+            else if (dbType == DbTypes.Oracle)
+            {
+                data = context.ExecuteProc<ArtUserPerformancePerActionUser>(ORACLESPName.ST_USER_PERFORMANCE_PER_ACTION_USER, summaryParams.ToArray());
+            }
+
+
+            KendoDataDesc<ArtUserPerformancePerActionUser> Data = data.AsQueryable().CallData(para.req);
+
+
+            var result = new
+            {
+                data = Data.Data,
+                columns = Data.Columns,
+                total = Data.Total,
+                reportname = "UserPerformancePerActionUser"
+            };
+            return new ContentResult
+            {
+                ContentType = "application/json",
+                Content = JsonConvert.SerializeObject(result, new JsonSerializerSettings
+                {
+                    ReferenceLoopHandling = ReferenceLoopHandling.Ignore,
+
+                }),
+
+            };
+        }
 
         public async Task<IActionResult> Export([FromBody] StoredReq para)
         {
