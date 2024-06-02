625564d794e798467f8994d660d65f6499ea6f36
MohammedAliMohammedElsady <m7md3ly2015@gmail.com>
2024-06-02 11:07:14 +0300

    change type of month in emp summary per month

--- Data/Data/DGINTFRAUD/ArtDgamlCasesTransactions.cs
+++ Data/Data/DGINTFRAUD/ArtDgamlCasesTransactions.cs
@@ -4,7 +4,7 @@
     {
         public string Employee_Number { get; set; }
         public string Employee_Name { get; set; }
-        public decimal? Month { get; set; }
+        public string Month { get; set; }
         public decimal? Amount { get; set; }
         public decimal? Count { get; set; }
         public decimal? Alerts_Count { get; set; }
