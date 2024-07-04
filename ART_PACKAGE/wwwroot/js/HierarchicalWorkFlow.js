2494f54a314d5ea7508d12c40b761be7729f19bc
MohammedAliMohammedElsady <m7md3ly2015@gmail.com>
2024-07-04 11:06:13 +0300

    test

--- ART_PACKAGE/wwwroot/js/HierarchicalWorkFlow.js
+++ ART_PACKAGE/wwwroot/js/HierarchicalWorkFlow.js
@@ -7,44 +7,45 @@
     // Show loader before data loading
     //$("#loader").show();
 
-    $("#orgchart").kendoOrgChart({
-        dataSource: {
-            transport: {
-                read: {
-                    url: "/HierarchicalWorkflow/GetData",
-                    dataType: "json"
-                }
-            },
-            schema: {
-                model: {
-                    id: "id",
-                    parentId: "parentId",
-                    expanded: true,
-                    fields: {
-                        id: { field: "id", type: "string", nullable: false },
-                        parentId: { field: "parentId", nullable: true },
-                        title: { field: "title", nullable: true },
-                        name: { field: "id" }
-                    }
-                }
-            },
-            requestEnd: function (e) {
-                // Hide spinner after data loading
-                //$("#loader").hide();
-            }
-        },
-        editable: false
-    });
+    //$("#orgchart").kendoOrgChart({
+    //    dataSource: {
+    //        transport: {
+    //            read: {
+    //                url: "/HierarchicalWorkflow/GetData",
+    //                dataType: "json"
+    //            }
+    //        },
+    //        schema: {
+    //            model: {
+    //                id: "id",
+    //                parentId: "parentId",
+    //                expanded: true,
+    //                fields: {
+    //                    id: { field: "id", type: "string", nullable: false },
+    //                    parentId: { field: "parentId", nullable: true },
+    //                    title: { field: "title", nullable: true },
+    //                    name: { field: "id" }
+    //                }
+    //            }
+    //        },
+    //        requestEnd: function (e) {
+    //            // Hide spinner after data loading
+    //            //$("#loader").hide();
+    //        }
+    //    },
+    //    editable: false
+    //});
 });
 
 
-//fetch("/HierarchicalWorkflow/GetData").then(x => x.json()).then(
-//    x => {
-//        var data = x;
-//        $("#orgchart").kendoOrgChart({
-//            dataSource: data,
-//            editable: false
-//        });
-//    }
-//);
+fetch("/HierarchicalWorkflow/GetData").then(x => x.json()).then(
+    x => {
+        var data = x;
+        console.log(data);
+        $("#orgchart").kendoOrgChart({
+            dataSource: data,
+            editable: false
+        });
+    }
+);
 
