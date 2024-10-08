e216d97a63ee141327a54bd596a118fa2cb79283
MohammedAliMohammedElsady <m7md3ly2015@gmail.com>
2024-10-08 18:39:57 +0300

    add sas aml sla report

---
+++ ART_PACKAGE/Controllers/SASAML/SlaClosedAlertsExceeded45DaysController.cs
@@ -0,0 +1,26 @@
+﻿using ART_PACKAGE.Areas.Identity.Data;
+using ART_PACKAGE.Helpers.Grid;
+using Data.Data.SASAml;
+using Data.Services;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ART_PACKAGE.Controllers.SASAML
+{
+    //////[Authorize(Roles = "SlaClosedAlertsExceeded45Day")]
+    public class SlaClosedAlertsExceeded45DaysController : BaseReportController<IGridConstructor<IBaseRepo<SasAmlContext, SlaClosedAlertsExceeded45Day>, SasAmlContext, SlaClosedAlertsExceeded45Day>, IBaseRepo<SasAmlContext, SlaClosedAlertsExceeded45Day>, SasAmlContext, SlaClosedAlertsExceeded45Day>
+    {
+        public SlaClosedAlertsExceeded45DaysController(IGridConstructor<IBaseRepo<SasAmlContext, SlaClosedAlertsExceeded45Day>, SasAmlContext, SlaClosedAlertsExceeded45Day> gridConstructor, UserManager<AppUser> um) : base(gridConstructor, um)
+        {
+        }
+
+
+
+        public override IActionResult Index()
+        {
+            return View();
+        }
+
+
+    }
+}
---
+++ ART_PACKAGE/Helpers/ReportsConfigurations/SASAML/SlaLevel2NonStaffViolatedWithActionConfig.cs
@@ -0,0 +1,28 @@
+﻿using Data.Services.Grid;
+
+namespace ART_PACKAGE.Helpers.ReportsConfigurations
+{
+    public class SlaLevel2NonStaffViolatedWithActionConfig : ReportConfig
+    {
+
+        public SlaLevel2NonStaffViolatedWithActionConfig()
+        {
+            DisplayNames = new Dictionary<string, GridColumnConfiguration>
+            {
+                { "AlertedEntityName", new GridColumnConfiguration { DisplayName = "Alerted Entity Name" } },
+                { "BranchName", new GridColumnConfiguration { DisplayName = "Branch Name" } },
+                { "BranchNumber", new GridColumnConfiguration { DisplayName = "Branch Number" } },
+                { "AlertStatus", new GridColumnConfiguration { DisplayName = "Alert Status" } },
+                { "LastActionDate", new GridColumnConfiguration { DisplayName = "Last Action Date" } },
+                { "AssignedDate", new GridColumnConfiguration { DisplayName = "Assigned Date" } },
+                { "InvestigationDays", new GridColumnConfiguration { DisplayName = "Investigation Days" } },
+                { "LevelOfRisk", new GridColumnConfiguration { DisplayName = "Level Of Risk" } },
+                { "UserName", new GridColumnConfiguration { DisplayName = "User Name" } },
+                { "AlertId", new GridColumnConfiguration { DisplayName = "Alert ID" } },
+                { "AlertedEntityNumber", new GridColumnConfiguration { DisplayName = "Alerted Entity Number" } }
+            };
+        }
+
+
+    }
+}
