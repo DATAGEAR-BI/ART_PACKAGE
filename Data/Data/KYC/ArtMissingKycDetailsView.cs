d1f2ddbbcc1e6b4fb7a5d251b95d538257892ae8
Ehab Azab <ehab.azab@datagearbi.com>
2024-11-10 11:29:28 +0200

    change columns order of missing kyc details report

--- Data/Data/KYC/ArtMissingKycDetailsView.cs
+++ Data/Data/KYC/ArtMissingKycDetailsView.cs
@@ -10,17 +10,17 @@ namespace Data.Data.KYC
     {
         public string Cid { get; set; } = null!;
         public string? IdentificationNumber { get; set; }
-        public DateTime? CustIdentExpDate { get; set; }
         public DateTime? DateOfBirthOrIncorporation { get; set; }
         public string? PhoneNumber { get; set; }
-        public string? KraPin { get;set; }
+        public string? KraPin { get; set; }
         public string? ForeignTaxIdentificationNumber { get; set; }
-        public string? Nationality {  get; set; }
-        public string? Residence {  get; set; }
-        public string? RiskStatusOrRiskClassification { get; set; }
-        public string? PepFlag { get; set; }
-        public string? NatureOfBusiness { get; set; }
+        public string? Nationality { get; set; }
+        public string? Residence { get; set; }
         public string? EmailAddress { get; set; }
+        public string? RiskStatusOrRiskClassification { get; set; }
+        public string? NatureOfBusiness { get; set; }
+        public DateTime? CustIdentExpDate { get; set; }
+        public string? PepFlag { get; set; }
         public string? DirectorIdentificationNumber { get; set; }
         public string? DirectorNationality { get; set; }
         public DateTime? DirectorDateOfBirth { get; set; }
@@ -30,12 +30,11 @@ namespace Data.Data.KYC
         public string? SignatoryIdentificationNumber { get; set; }
         public string? SignatoryNationality { get; set; }
         public DateTime? SignatoryDateOfBirth { get; set; }
-        public string? SignatoryPhoneNumber { get;set; }
-        public string? SignatoryPin { get;set; }
-        public string? SignatoryOccupation { get;set; }
-        public string? UboName { get;set; }
-        public string? UboId { get;set; }
-
+        public string? SignatoryPhoneNumber { get; set; }
+        public string? SignatoryPin { get; set; }
+        public string? SignatoryOccupation { get; set; }
+        public string? UboName { get; set; }
+        public string? UboId { get; set; }
 
     }
 }
