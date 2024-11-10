910b0006f224ea17a32e1b97ef06ab36d8a43e73
Ehab Azab <islamkhalil314@gmail.com>
2024-11-10 18:16:02 +0200

    adding pdf settings

---
+++ Data/Setting/PDF.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data.Setting
+{
+    public class PDF
+    {
+
+        public bool IsLandScape { get; set; }
+        public int IsNumberOfRowsInPage { get; set; }
+        public int NumberOfColumnsInPage { get; set; }
+    }
+}
