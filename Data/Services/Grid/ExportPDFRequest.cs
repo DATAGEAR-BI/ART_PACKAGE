dd1f196ce2df327653c127b0317462c52c25ca19
Ehab Azab <islamkhalil314@gmail.com>
2024-11-10 18:15:50 +0200

    adding properties for pdf exporting

---
+++ Data/Services/Grid/ExportPDFRequest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data.Services.Grid
+{
+    public  class ExportPDFRequest:ExportRequest
+    {
+        public bool? IsLandscape { get; set; }=false;
+        public int? NumberOfRowsInPage { get; set; }
+        public int? NumberOfColumnsInPage { get; set; }
+
+    }
+}
